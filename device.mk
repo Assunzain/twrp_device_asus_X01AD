c98a0e6e0cda69c56f2b39a4ea8991ca9cf83df2
Rocky7842 <eric.rocky7842@gmail.com>
2023-12-04 22:42:31 +0800

    X01AD: Introduce FBE support
    * Using qcom_decrypt_fbe from qcom twrp common tree.

--- device.mk
+++ device.mk
@@ -7,6 +7,14 @@
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 27
 
+# Crypto
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keymaster_ver=3.0
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
