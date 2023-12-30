5ede251208caa14ec7be1715504f0b489d5a28f5
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-12-30 14:04:05 +0700

    Initial build pbrp

--- twrp_X01AD.mk
+++ twrp_X01AD.mk
@@ -18,8 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
