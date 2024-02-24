0fe3e9767dcb5d379543936e70d750fc33d56401
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-02-24 21:31:18 +0700

    Initial build ofox12.1

--- twrp_X01AD.mk
+++ twrp_X01AD.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
