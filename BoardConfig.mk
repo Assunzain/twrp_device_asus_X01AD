9abb26a3ea86a123792cb4f5ddfd7f6373f6d5a9
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-02-24 22:58:24 +0700

    Add TW_MAX_brightness

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,6 +100,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 60 # For a max brightness of 255
+TW_MAX_BRIGHTNESS := 255
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone6/temp
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
